5295be5091dee00eb789840ef271c28c2e124989
Alex Javier <alexavier1989@gmail.com>
2024-12-16 23:14:53 -0500

    feat: Tasks part 1 solved

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
-// Prevent us from attempting to use variables
-// that are not declared
 "use strict"
+// const devName = "Linus Torvalds";
+// const devNameTypeOf = typeof devName;
+// console.log(`The name is "${devName}" and it's a "${devNameTypeOf}"`) 
+// // Output: The name is "Linus Torvalds" and it's a "string"
+
+
+// const devBirthYear = 1969;
+// const currentYear = new Date().getFullYear();
+// const devAge = currentYear - devBirthYear;
+// console.log(`The dev was born in ${devBirthYear}, so he/she turns ${devAge} this year, and this is a "${typeof devAge}"`)
+// // Output: The dev was born in 1969, so he/she turns 55 this year, and this is a "number"
+
+// let isMentor = true;            
+// console.log(isMentor)           // Output: true
+// console.log(typeof isMentor)    // Output: "boolean"
+
+// isMentor = false;
+
+// console.log(isMentor)           // Output: false
+// console.log(typeof isMentor)    // Output: "boolean"
+
+// const definedVariable = "example1";
+// console.log(definedVariable)           // Output: example1
+// console.log(typeof undefinedVariable)  //Output: undefined
+
+// const nullValue =  null;
+// console.log(nullValue)           // Output: null
+// console.log(typeof nullValue)  //Output: object
 
