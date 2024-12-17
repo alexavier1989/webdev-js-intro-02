3c2706e638226a55d6875d6ffa89006c38e1e426
Alex Javier <alexavier1989@gmail.com>
2024-12-16 23:37:45 -0500

    feat: tasks part 2 done. adding javascript code

--- js/assignment.js
+++ js/assignment.js
@@ -1,35 +1,35 @@
 "use strict";
 
-// Declaring constant variables to store references to the elements
-// that will be updated by your code.
 const yearElement = document.getElementById("year");
 const resolutionElement = document.getElementById("resolution")
 const willMeetResolutionElement = document.getElementById("will-meet-resolution")
 const submissionBtn = document.getElementById("submission-btn")
 
-// Declare your variables here.
+const resolution = "I will get a remote job as Web Developer and work from the beach"
+const currentYear = 2025 // new Date().getFullYear();
+let willMeetResolution = true
 
 function updateYear() {
-    // Update this function
-
+    yearElement.innerHTML = currentYear;
 }
 
 function updateResolution() {
-    // Update this function 
-
+    resolutionElement.innerHTML = resolution; 
 }
 
 function updateWillMeetResolution() {
-    // Update this function
+    console.log(willMeetResolutionElement);
+    willMeetResolutionElement.innerHTML = willMeetResolution; 
 
 }
 
 function render() {
-    // Update this function
-
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
-    // Update this function
+    render();
 
 })
